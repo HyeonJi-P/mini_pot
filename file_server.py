f5aae1bd3914268241729bc01f2f60058e696b73
HyeonJi-P <hyunji7674@gmail.com>
2021-05-26 19:05:29 +0900

    전체를 while로 감쌌음

--- file_server.py
+++ file_server.py
@@ -2,25 +2,25 @@ import socket
 from os.path import exists
 import sys
 
-try:
-    server_s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # ipv4, tcp 형식 사용
-    print("소켓 생성")
-except socket.error as e :
-    print("***** 서버 소켓 생성 에러발생 *****")
-    print("원인 : " + e)
-
-HOST = 'ec2-52-79-235-57.ap-northeast-2.compute.amazonaws.com'
-#'ec2-15-165-203-96.ap-northeast-2.compute.amazonaws.com'
-
-PORT = 8282
-server_s.bind((HOST,PORT))  # 소켓을 호스트와 포트에 연결
-server_s.listen(2)  # 클라이언트에게 들을 준비 완료 (동시접속 2허용)
-
-client_s, address = server_s.accept()  # accept에서 대기하다 client 나타나면 새로운 소켓 리턴
-
-print("연결 완료 : ", address)
-
 while 1:
+    try:
+        server_s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # ipv4, tcp 형식 사용
+        print("소켓 생성")
+    except socket.error as e :
+        print("***** 서버 소켓 생성 에러발생 *****")
+        print("원인 : " + e)
+
+    HOST = 'ec2-52-79-235-57.ap-northeast-2.compute.amazonaws.com'
+    #'ec2-15-165-203-96.ap-northeast-2.compute.amazonaws.com'
+
+    PORT = 8282
+    server_s.bind((HOST,PORT))  # 소켓을 호스트와 포트에 연결
+    server_s.listen(2)  # 클라이언트에게 들을 준비 완료 (동시접속 2허용)
+
+    client_s, address = server_s.accept()  # accept에서 대기하다 client 나타나면 새로운 소켓 리턴
+
+    print("연결 완료 : ", address)
+
 
     print(str(address),'에서 접속')
     # 클라이언트 메세지 수신 대기 
