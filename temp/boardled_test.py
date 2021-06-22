af9c7aab375ee410e69b5c94137b7b2b20e371e8
Auint <44167182+Auint@users.noreply.github.com>
2021-06-22 15:48:10 +0900

    나눠서 테스트

--- temp/boardled_test.py
+++ temp/boardled_test.py
@@ -1,5 +1,3 @@
-# 나중에 쓰일수 있는 계륵같은 코드
-
 import RPi.GPIO as GPIO
 import spidev
 import time
--- temp/sql_test.py
+++ temp/sql_test.py
@@ -9,12 +9,12 @@ def test():
     cur.execute(sql)
     conn.commit()
 
-    sql = "INSERT INTO mytable(time, plant, temperature, humidity, illuminance) VALUES('2021-06-22 14:48:11.003', 'temp_plan', 6, 7, 8.88);"
-    cur.execute(sql)
-    conn.commit()
+    #sql = "INSERT INTO mytable(time, plant, temperature, humidity, illuminance) VALUES('2021-06-22 14:48:11.003', 'temp_plan', 6, 7, 8.88);"
+    #cur.execute(sql)
+    #conn.commit()
 
-    rows = cur.fetchall()
-    print(rows)
+    #rows = cur.fetchall()
+    #print(rows)
 
 
     conn.close()
