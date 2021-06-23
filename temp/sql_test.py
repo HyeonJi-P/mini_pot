f9330e16d3a495465577f26c59c9491c57b77e67
Auint <44167182+Auint@users.noreply.github.com>
2021-06-23 09:38:41 +0900

    열씨미 일을하내 뚜뚜

--- temp/sql_test.py
+++ temp/sql_test.py
@@ -5,17 +5,19 @@ def test():
     db = 'mysql', charset='utf8')
     cur = conn.cursor()
 
+    '''데이터 추출 하는중'''
     query = "SELECT * FROM mytable;"
     cur.execute(query)
+    result = cur.fetchall()
     conn.commit()
 
-    print()
+    print(result)
 
-
-
+    ''' 데이터 삽입 완료
     query = "INSERT INTO mytable(time, plant, temperature, humidity, illuminance) VALUES('2021-06-22 16:31:11.003', 'temp_plan', 6, 7, 8.88);"
     cur.execute(query)
     conn.commit()
+    '''
 
     conn.close()
 
