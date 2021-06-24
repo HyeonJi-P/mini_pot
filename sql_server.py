8dae43fc1fac1a5394622ba834c90a6ff016ca1a
Auint <44167182+Auint@users.noreply.github.com>
2021-06-24 13:45:16 +0900

    흐ㅓ허허허허허허헝

--- sql_server.py
+++ sql_server.py
@@ -6,22 +6,113 @@ class sql_server:
 
     @staticmethod
     def insert(insert_data):
-        conn = pymysql.connect(host='localhost', port=3306, user='auint', password='*6CC6A1C22CFFA93B23769CAE343636557E024D12', 
-        db = 'mysql', charset='utf8')
+        conn = pymysql.connect(host='localhost', port=3306, user='auint', password='pwpw', 
+        db = 'mysql', charset='utf8')  # *6CC6A1C22CFFA93B23769CAE343636557E024D12
 
         try:
-            # cur = conn.cursor()
+            # cur = conn.cursor()랑 같은 의미
             with conn.cursor() as cur:
 
+                # DB에 쿼리로 데이터 추가
                 col = ','.join(insert_data.keys())
                 val = ','.join(insert_data.values())
-                sql = "INSERT INTO mytable(%s) VALUSE(%s);" %(col, val)
-                cur.execute(sql, insert_data.values())
-                conn.commit()
+                query = "INSERT INTO mytable(%s) VALUSE(%s);" %(col, val)
+                cur.execute(query, insert_data.values())
 
+                conn.commit()
         finally:
             conn.close()
 
     @staticmethod
-    def select():
-        conn = 1
+    def select(where_data):
+        conn = pymysql.connect(host='localhost', port=3306, user='auint', password='pwpw', 
+        db = 'mysql', charset='utf8')
+
+        try:
+            with conn.cursor() as cur:
+
+                # 들어오는 명렁어 추가 해서 검색하기 
+                add_query = ""
+                if where_data is not None:
+                    add_query = " WHERE "
+                    add_query += where_data
+
+                # 검색, 결과도출 
+                query = "SELECT * FROM mytable" + add_query + ";"
+                result_query = pd.read_sql(query, con = conn)
+                
+                # 전송을 위해 dataframe -> dict으로 변환
+                df = df.to_dict('records')  # 형식: {time, plant..}, {time, plant...}
+                # 받고나서 다시 dict -> dataframe로 변환 [df = pd.DataFrame(df)]
+
+                print(df)  # 테스트
+
+                ''' ex) WHERE
+                plant='hub' // 식물이 허븬거 찾기
+                plant LIKE 'temp%' // temp*인거 모두 ex) temp_plan, temp_abcdf...
+                time BETWEEN '2021-06-22 14:00:00' AND '2021-06-22 14:40:00' // ~부터 ~까지 
+                temperature < 7
+                '''
+
+                conn.commit()
+        finally:
+            conn.close()
+
+    @staticmethod
+    def delete(delete_data):
+        conn = pymysql.connect(host='localhost', port=3306, user='auint', password='pwpw', 
+        db = 'mysql', charset='utf8')
+
+        try:
+            with conn.cursor() as cur:
+
+                
+
+
+
+                conn.commit()
+        finally:
+            conn.close()
+
+    @staticmethod
+    def update(update_data):
+        conn = pymysql.connect(host='localhost', port=3306, user='auint', password='pwpw', 
+        db = 'mysql', charset='utf8')
+
+        try:
+            with conn.cursor() as cur:
+
+                
+
+
+
+                conn.commit()
+        finally:
+            conn.close()
+
+
+# now test
+
+dict_message = {
+    'time' : '1111-22-33 44:55:66',
+    'plant' : 'baechu',
+    'temperature': 21,
+    'humidity': 6,
+    'illuminance': 24.13
+}
+
+sql_server.insert(dict_message)
+
+sql_server.select(None)
+
+'''
+sql_server.select()
+
+sql_server.select("")
+
+sql_server.select("")
+'''
+
+
+
+
--- temp/grammar_test.py
+++ temp/grammar_test.py
@@ -52,19 +52,25 @@ print(type(a[0]))
 print(a[0])
 '''
 
-# sql문 받은 데이터 변환하기
+# sql 쿼리 문자열 처리하기
 ''''''
-import datetime
-import pandas as pd
-
-df = [(datetime.datetime(2021, 6, 22, 14, 25, 11), 'temp_plan', 6, 7, 8.88),
-(datetime.datetime(2021, 6, 22, 14, 36, 11), 'temp_plan', 6, 7, 8.88),
-(datetime.datetime(2021, 6, 22, 14, 48, 11), 'temp_plan', 6, 7, 8.88),
-(datetime.datetime(2021, 6, 22, 16, 31, 11), 'temp_plan', 6, 7, 8.88),
-(datetime.datetime(2021, 6, 23, 10, 53, 11), 'hub', 9, 10, 11.11)]
+class a:
+    @staticmethod
+    def insert(insert_data):
+        print(insert_data)
 
 
-print(df)
+dict_message = {
+    'time' : '1111-22-33 44:55:66',
+    'plant' : 'baechu',
+    'temperature': 21,
+    'humidity': 6,
+    'illuminance': 24.13
+}
+
+a.insert(dict_message)
+
+
 
 
 
