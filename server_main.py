40be671cacb3df4bec7f66308fe16b064dfa3591
Auint <44167182+Auint@users.noreply.github.com>
2021-06-28 14:40:28 +0900

    server_main design... 조금 복잡하냉

--- server_main.py
+++ server_main.py
@@ -2,15 +2,23 @@ import time
 import json
 import pandas as pd
 
+from sql_server import *
 
+print("!! process start")
 try:
     while 1:
 
-
-
-
-
+        # 데이터 수신
         
+
+        # 데이터 저장
+        # 데이터 가공
+
+
+        # 데이터 송신
+
+
+
         # 지금은 1회 테스트 
         break
 
