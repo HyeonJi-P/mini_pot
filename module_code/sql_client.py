11b50c52706162734b65d47fde5c320ecc696aa1
Auint <44167182+Auint@users.noreply.github.com>
2021-06-28 10:55:38 +0900

    module

--- module_code/sql_client.py
+++ module_code/sql_client.py
@@ -2,17 +2,16 @@ import pymysql
 
 class sql_client:
     def insert(insert_data):
-        conn = pymysql.connect(host='localhost', port=3306, user='root', password='*6CC6A1C22CFFA93B23769CAE343636557E024D12', 
+        conn = pymysql.connect(host='localhost', port=3306, user='auint', password='', 
         db = 'mysql', charset='utf8')
 
         try:
-            # cur = conn.cursor()
             with conn.cursor() as cur:
 
-                col = ','.join(insert_data.keys())
-                val = ','.join(insert_data.values())
-                sql = "INSERT INTO mytable(%s) VALUSE(%s);" %(col, val)
-                cur.execute(sql, insert_data.values())
+                
+
+                
+                cur.execute()
                 conn.commit()
 
         finally:
