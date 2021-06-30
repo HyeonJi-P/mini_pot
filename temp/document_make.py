dcc8b9f779af6712c5af1bf4258483cccf79615c
Auint <44167182+Auint@users.noreply.github.com>
2021-06-30 17:58:49 +0900

    word-양식에 대한 대입방벙 추가해야함, pdf화 할시 import win32 문제

--- temp/document_make.py
+++ temp/document_make.py
@@ -3,6 +3,13 @@ from docx.oxml.ns import qn # 한글 폰트
 from docx.enum.text import WD_ALIGN_PARAGRAPH # 중앙정렬
 from docx.shared import Cm, Inches # 이미지 삽입시 길이 단위
 
+import time # 파일생성시 이름에 날짜로 정렬
+
+from docx2pdf import convert# docx를 pdf로 변환
+import os
+import sys
+#import win32api
+
 #from fpdf import FPDF
 #import pandas as pd
 
@@ -10,27 +17,30 @@ from docx.shared import Cm, Inches # 이미지 삽입시 길이 단위
 '''
 * 호출시 임포트
 import docx
-from docx.oxml.ns import qn # 한글 폰트
 
 * 새로운 임포트 설치
 pip3 install python-docx
+pip3 install docx2pdf
+pip3 install pypiwin32
+pip3 install pywin32
+
+conda install -c anaconda pywin32
+
 '''
 
 class document_make:
     # 함수종류
-    ### word : docx 생성
+    ### word : docx 생성 (미사용)
+    ### word_form : 미리 정해진 양식을 사용하여 docx 생성
     ### convert : docx를 pdf로 변환
 
     # 전체적인 구조
-    ### 데이터 형식을 받아서 docx를 생성
-    ### docx를 pdf로 변환해서 송신부에 전달
+    ### docx형식 파일을 불러와서 받은 데이터를 대입시킴
+    ### docx를 pdf로 변환
 
     @staticmethod
     def word(insert_data):
-        print("START")
         doc = docx.Document()
-
-        # ++ 다하고 변수로 다 해서 데이터 전달만 되면 바로 생성되게 만들기
 
         para = doc.add_paragraph()
         run = para.add_run('mini pot - title') # +본문
@@ -51,29 +61,66 @@ class document_make:
         doc.add_paragraph('첫번째 단락', style='List Bullet')
         doc.add_paragraph('첫번째 순서 단락', style='List Number')
 
-        # https://python-docx.readthedocs.io/en/latest/
-        ## 참고 후 추가
-        ## ++ 헤드, 나누는선 꾸미기
+        # 공문서 : https://python-docx.readthedocs.io/en/latest/
 
-        # ++ 사실상 여기에서 표를 만들면 쌩고생이기 때문에
-        # 정보 가공부분에서 이미지로 가공해서 넘겨주면 여기서 그걸 사용하는게 제일 좋아보임
-        # (보여주는 타입을 2개로 나누어서 일반:그래프, 고급:표 로)
-
-
-        # ++파일 이름 지정
-        # 년월일-시분_식물이름 ex) 210629-165300_hub
-        # 나중에 가능하다면 ex) 식물애칭-time 
-
-        doc.save("time_plant.docx") 
-        print("END")
+        doc.save("time_plant.docx")
 
     @staticmethod
-    def convert(insert_data):
-        a = 1
-        # docx to pdf 추후 참고
-        # https://passing-story.tistory.com/228
-        # https://pythonq.com/so/python/675830
+    def word_form(insert_data):
+        print("START")
+        doc = docx.Document("report_form.docx")
+
+        title = "mini pot"
+        user = "ZIMyMeMine"
+        plant = "T-hub"
+
+        now_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
+
+        # 제목 대입 
+        ## doc.paragraphs[0].text = title // 한줄로
+        ###doc.add_heading("mini-pot", 0) // 단순 생성
+        para = doc.paragraphs
+        para[0].text = title
+        
+        
+        #doc.add_paragraph("ttap", style = 'a') // 스타일이 a인 내용 ttap를 생성
+
+        #para = doc.add_paragraph() // 해당 라인의 뒤에 추가로 생성
+        #run = para.add_run('mini pot - title')
+
+        # ++ 보여주는 타입을 2개로 나누어서 일반:그래프, 고급:표
+
+
+
+        # 디버깅 : word 문서를 번호 : 내용으로 확인
+        for x, paragraph in enumerate(doc.paragraphs):
+            print(str(x) + " : " + paragraph.text)
+
+        # 생성 날짜와 유저,식물 정보로 사진을 저장 
+        time_file = time.strftime('%Y%m%d-%H%M%S', time.localtime(time.time()))
+        file_name = user + "_" + time_file + "_" + plant
+        doc.save(file_name + ".docx")
+        print("END")
+
+        document_make.convert_pdf(file_name)
+
+    @staticmethod
+    def convert_pdf(convert_file_name):
+
+        # !! 절대 경로시 \\ 이렇게 두번 해줘야함 
+        
+        docx = convert_file_name + ".docx"
+        pdf = convert_file_name + ".pdf"
+        docx_path = ".\\"
+        pdf_path = ".\\"
+
+        print(docx_path+docx)
+        print(pdf_path+pdf)
+
+        convert(docx_path+docx, pdf_path+pdf)
+
+        
 
 # test space ----------------------------------------
 ''''''
-document_make.word(1)
+document_make.word_form(1)
