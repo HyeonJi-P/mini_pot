8f1c2953c4898de88a59b9763bd9e8b0f3c3b725
HyeonJi-P <hyunji7674@gmail.com>
2021-08-08 15:47:22 +0900

    온도 그래프 예시 설명추가

--- report_graph.py
+++ report_graph.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import matplotlib.font_manager as fm
 
+######설치안된것들은 'pip install 설치할것' 으로 커멘드
+
 #csv파일 읽기
 temp_all = pd.read_csv("./mini_pot/temperature.csv", encoding='cp949')
 
@@ -20,18 +22,16 @@ for data in temp_del['Date']:
 temp_del['month']=month
 temp_del['day']=day
 
-temp_del['month'].astype('int64')
+temp_del['month'].astype('int64') #필요한지 몰라서 일단 썼음
 temp_del['day'].astype('int64')
 
 print(temp_del)
 
 # seaborn의 countplot 함수를 사용하여 출력
 #한국어 출력은 폰트설정 필요
-order =[]
-for i in range(1,32):
-    order.append(str(i))
-plt.figure(figsize=(10,5))
-sns.set(style="darkgrid")
+
+plt.figure(figsize=(10,5)) #표 사이즈
+sns.set(style="darkgrid") #스타일 설정
 ax = sns.lineplot(x='day',y='mean', data=temp_del) #평균기온
 ax = sns.lineplot(x='day',y='lowest', data=temp_del) #최저기온
 ax = sns.lineplot(x='day',y='highest', data=temp_del) #최고기온
